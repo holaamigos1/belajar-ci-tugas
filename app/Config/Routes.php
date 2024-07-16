ebf00f0af84505acc64cb1b593a9dfdecdf17f33
holaamigos1 <mahendrareza50@gmail.com>
2024-07-16 19:21:51 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -28,6 +28,11 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('getcity', 'TransaksiController::getcity', ['filter' => 'auth']);
+$routes->get('getcost', 'TransaksiController::getcost', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
 $routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
